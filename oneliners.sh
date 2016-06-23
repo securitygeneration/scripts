43d71644bf2278c2adb74e3ba54582e6316263a7
Security Generation <info@securethoughts.net>
2016-06-23 11:49:29 +0200

    Update oneliners.sh

--- oneliners.sh
+++ oneliners.sh
@@ -1,10 +1,10 @@
-# Bash one-liners to do random things
+### Bash one-liners to do random things
 
-# Extract IP, prot, port, service and script name
+### Extract IP, protocol, port, service, NSE script name and script output from nmap.xml file ###
 # 10.11.18.197 udp 161 snmp snmp-brute 
 #   public - Valid credentials
 #   private - Valid credentials
 
 xmlstarlet sel -t -m "//host/ports/port[service[@name='snmp']]/script" -v "concat(ancestor::host/address[@addrtype='ipv4']/@addr,' ', ../@protocol,' ', ../@portid,' ', ../service/@name,' ',@id,' ',str:replace(@output,'&#xa;',' '))" -n snmp_brute.xml
 
-##############
+###
