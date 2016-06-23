e08609d1b60a9b48764280e33184c2ea44c6feeb
Security Generation <info@securethoughts.net>
2016-06-23 11:50:30 +0200

    Update oneliners.sh

--- oneliners.sh
+++ oneliners.sh
@@ -1,6 +1,7 @@
 ### Bash one-liners to do random things
 
 ### Extract IP, protocol, port, service, NSE script name and script output from nmap.xml file ###
+# Sample output:
 # 10.11.18.197 udp 161 snmp snmp-brute 
 #   public - Valid credentials
 #   private - Valid credentials
